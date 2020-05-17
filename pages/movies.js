489f0767810cd9cc8ab6d246e511a48a15718632
Subhadeep95 <subhadeep.code@gmail.com>
2020-05-17 20:45:57 +0530

    added the dropbox link for cloverfield

--- pages/movies.js
+++ pages/movies.js
@@ -2,9 +2,12 @@
 
 const Movies = () => {
 	return (
-		<div>
-			<iframe src="https://drive.google.com/file/d/1sv0f5r5KbNgn0BdTIWBHQuy0VfaB4xvz/preview" width="640" height="480"></iframe>
-		</div>
+		// <div>
+		// 	<iframe src="https://drive.google.com/file/d/1sv0f5r5KbNgn0BdTIWBHQuy0VfaB4xvz/preview" width="640" height="480"></iframe>
+		// </div>
+		<video width="600px" height="auto" controls="true">
+			<source src="https://www.dropbox.com/s/mz04do02x4wfrcx/10%20Cloverfield%20Lane%202016%20720p.mp4?dl=1" type="video/mp4" />
+			</video>
 		)
 }
 
