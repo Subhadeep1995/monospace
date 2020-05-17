e77e31641f411bf110d467d231c9f6946610dbee
Subhadeep95 <subhadeep.code@gmail.com>
2020-05-17 17:36:44 +0530

    final working build

--- pages/x-men/x-men-s03.js
+++ pages/x-men/x-men-s03.js
@@ -6,7 +6,7 @@ const XmenHome = () => {
 			<h1>X-men</h1>
 			<ul>
 				<li><Link href="x-men-s03-e03">X-men S03E03</Link></li>
-				<li><Link href="x-men-s03-e04">X-men S03E03</Link></li>
+				<li><Link href="x-men-s03-e04">X-men S03E04</Link></li>
 			</ul>
 		</div>
 	)
